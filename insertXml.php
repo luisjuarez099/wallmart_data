68319d5d6249cf0101fd75379cd71cd91203846c
luis juarez <luisjuarezcc9@gmail.com>
2023-06-21 21:01:02 -0600

    Archivo que lee el StoreProcedure y e inserta en la BD

--- insertXml.php
+++ insertXml.php
@@ -4,8 +4,8 @@ $servername = "localhost";
 $password = "";
 $username = "root";
 
-$conn = mysqli_connect($servername, $username, $password, $database); //variable de conexion a la BD
-$ruta = "0000-00-00employeeData.xml"; //ruta del archivo xml 
+$conn =new mysqli($servername, $username, $password, $database); //variable de conexion a la BD
+$ruta = "2023-06-12employeeData.xml"; //ruta del archivo xml 
 if (!$conn) {
     die("Conexion fallo: " . mysqli_connect_error());
 }
@@ -13,8 +13,9 @@ if (!$conn) {
 
 $employees = simplexml_load_file($ruta); //Interprets an XML file into an object
 foreach ($employees as $employee) {
+    //Imprimir en formato de tabla
     echo "<tr>";
-        echo "<td> " . $employee->OC . " </td>";
+        echo "<td> " . $employee->OC . "</td>";
         echo "<td> " . $employee->Proveedor . " </td>";
         echo "<td> " . $employee->Producto . " </td>";
         echo "<td> " . $employee->Descripcion . " </td>";
@@ -23,14 +24,35 @@ foreach ($employees as $employee) {
         echo "<td> " . $employee->Precio . " </td>";
     echo "</tr>";
 
-    mysqli_query($conn ,"SET @p1='".$employee->OC."'");
-    mysqli_query($conn ,"SET @p2='". $employee->Proveedor."'");
-    mysqli_query($conn ,"SET @p3='".$employee->Producto."'");
-    mysqli_query($conn ,"SET @p4='".$employee->Descripcion."'");
-    mysqli_query($conn ,"SET @p5='".$employee->Cantidad ."'");
-    mysqli_query($conn ,"SET @p6='".$employee->FechaEntrega."'");
-    mysqli_query($conn ,"SET @p7='".$employee->Precio."'");
-    mysqli_multi_query ($conn, "CALL insert_wm (@p1,@p2,@p3,@p4,@p5,@p6,@p7 )") OR DIE (mysqli_error($conn));
+    mysqli_query($conn, "SET @p0='" . $employee->OC . "'");
+    mysqli_query($conn, "SET @p1='" . $employee->Proveedor . "'");
+    mysqli_query($conn, "SET @p2='" . $employee->Producto . "'");
+    mysqli_query($conn, "SET @p3='" . $employee->Descripcion . "'");
+    mysqli_query($conn, "SET @p4='" . $employee->Cantidad . "'");
+    mysqli_query($conn, "SET @p5='" . $employee->FechaEntrega . "'");
+    mysqli_query($conn, "SET @p6='" . $employee->Precio . "'");
+    mysqli_multi_query($conn, "CALL insert_wm (@p0,@p1,@p2,@p3,@p4,@p5,@p6)");
+    while (mysqli_more_results($conn)) {
 
-    
+        if ($result = mysqli_store_result($conn)) {
+
+            while ($row = mysqli_fetch_assoc($result)) {
+
+                // i.e.: DBTableFieldName="userID"
+            echo "<tr>";
+                echo "<td> " . $row["OC"] ."</td>";
+                echo "<td> " . $row["Proveedor"] ."</td>";
+                echo "<td> " . $row["Producto"] ."</td>";             
+                echo "<td> " . $row["Descripcion"] ."</td>";
+                echo "<td> " . $row["Cantidad"] ."</td>";
+                echo "<td> " . $row["FechaEntrega"] ."</td>";
+                echo "<td> " . $row["Precio"] ."</td>";
+            echo "</tr>";
+            }
+            mysqli_free_result($result);
+        }
+        mysqli_next_result($conn);
+
+    }
+
 }
