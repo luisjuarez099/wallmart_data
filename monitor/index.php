310d714a3dfd9f74b32c40e6d902748e65d7f393
luis juarez <luisjuarezcc9@gmail.com>
2023-07-21 12:30:44 -0600

    se a agregado el envio de correos

--- monitor/index.php
+++ monitor/index.php
@@ -9,46 +9,64 @@ $conn = new mysqli($host, $username, $passwd, $dbname);
 
 
 
-$cont ="SELECT COUNT(*) FROM count_trigger";
+$cont = "SELECT COUNT(*) FROM count_trigger";
 // Ejecutar la consulta
 $resultado = $conn->query($cont);
 
 // Verificar si la consulta se ejecutó correctamente
 if ($resultado) {
-    // Obtener el valor del resultado
-    $row = $resultado->fetch_assoc();
-    $totalRegistros = $row['COUNT(*)'];
+  // Obtener el valor del resultado
+  $row = $resultado->fetch_assoc();
+  $totalRegistros = $row['COUNT(*)'];
+  //envio de correo 
+  
+  $comando = ' ssmtp luisjuarezcc9@gmail.com < ../demo_data/mail.txt'; // Ejemplo de comando (listar archivos y directorios)
 
-    echo "El número total de registros en la tabla count_trigger es: " . $totalRegistros;
+  // Ejecutar el comando y obtener la salida
+  $output = shell_exec($comando);
+
+  // Imprimir la salida del comando
+  
+
+  echo "El número total de registros en la tabla count_trigger es: " . $totalRegistros;
 } else {
-    echo "Error al ejecutar la consulta: " . $conn->error;
+  echo "Error al ejecutar la consulta: " . $conn->error;
 }
 ?>
 
 <html>
-  <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var totalRegistro = <?php echo $totalRegistros; ?>;
-        var data = google.visualization.arrayToDataTable([
-          ['bien', 'inertados correctamente'],
-          ['bien',     totalRegistro],
-        ]);
 
-        var options = {
-          title: 'My Daily Activities',
-          is3D: true,
-        };
+<head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", { packages: ["corechart"] });
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var totalRegistro = <?php echo $totalRegistros; ?>;
+      var data = google.visualization.arrayToDataTable([
+        ['bien', 'inertados correctamente'],
+        ['bien', totalRegistro],
+      ]);
 
-        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-        chart.draw(data, options);
-      }
-    </script>
-  </head>
-  <body>
-    <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
-  </body>
+      var options = {
+        title: 'My Daily Activities',
+        is3D: true,
+      };
+
+      var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+      chart.draw(data, options);
+    }
+
+    
+  </script>
+</head>
+
+<body>
+  <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
+  <script>
+    var mensajeCorreo="Se a enviado el correo exitosamente";
+    alert(mensajeCorreo);
+  </script>
+</body>
+    
 </html>
